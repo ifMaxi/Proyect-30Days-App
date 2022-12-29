706da3d4cd4594deeb8a557e81f058fa5eb0ef5f
ifMaxi <maxi96p@gmail.com>
2022-12-29 15:36:08 -0300

    Unused imports cleared

--- app/src/main/java/com/example/thirtydaysapp/DaysHome.kt
+++ app/src/main/java/com/example/thirtydaysapp/DaysHome.kt
@@ -8,7 +8,6 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -19,14 +18,11 @@ import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.thirtydaysapp.data.DaysFakeData
 import com.example.thirtydaysapp.model.DaysClass
 import com.example.thirtydaysapp.ui.theme.ThirtyDaysAppTheme
@@ -73,9 +69,11 @@ fun DaysHome(
  *
  * - Title with the day and its number.
  * - Subtitle with the name of the tasks.
+ * - An expansion button.
  * - An illustrative image.
  * - A text describing the task.
  *
+ * @param [daysInformation] Data class call.
  * @param [modifier] to beautify this function.
  */
 @Composable
